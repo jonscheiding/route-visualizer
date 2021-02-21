25b91022faa68fed1c11f5c1084cf4df876e1964
Jon Scheiding <jonscheiding@gmail.com>
2021-02-21 15:16:54 -0500

    Initial implementation of Leaflet

---
+++ src/Map.js
@@ -0,0 +1,17 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './Map.css';
+
+function Map() {
+  const position = [51.505, -0.09]
+
+  return (
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
+}
+
+export default Map;

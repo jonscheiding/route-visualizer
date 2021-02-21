5241c68c57189a21f0fca1bd2eec66f19538c18f
Jon Scheiding <jonscheiding@gmail.com>
2021-02-21 15:28:36 -0500

    Implementing a route with GeoJSON

--- src/Map.js
+++ src/Map.js
@@ -1,15 +1,18 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
+
 import './Map.css';
+import Route from './Route';
 
 function Map() {
-  const position = [51.505, -0.09]
+  const position = [50.2, 11.7833333]
 
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={position} zoom={11} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Route />
     </MapContainer>
   );
 }
